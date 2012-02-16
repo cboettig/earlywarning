e9cca1dbcc51225c88f89a9bab9535060bb1efad
Carl Boettiger <cboettig@gmail.com>
2012-02-15 17:35:17 -0800

    summary stat looping function, dependencies added

---
+++ R/summary_statistic.R
@@ -0,0 +1,28 @@
+
+#' apply a summary statistic over a set of replicate data
+#' 
+#' @param dat a data frame with columns "time", "reps", and "value"
+#' observed in a timeseries of replicates.   
+#' @param indicator a function taking X and windowsize, such as \link{window_var}
+#' @param windowsize, passed to indicator function
+#' @param ... additional optional arguments to the indicator function
+#' @return a data frame with columns "time", "reps", and "value",
+#' where values are the sliding-window computed summary statistics produced
+#' by indicator.  
+#' @details helper function just helps simple indicator functions be 
+#' applied to data formated with replicate observations and still
+#' return nicely formatted data 
+#' @import plyr
+#' @import reshape2
+#' @export
+summary_statistic <- function(dat, indicator, windowsize, ...){
+  tmp <- ddply(dat, "reps", function(X) 
+    indicator(X$value, windowsize=windowsize, ...))
+  stat <- melt(t(tmp))
+  stat <- stat[-1,]
+  names(stat) <- c("time", "reps", "value")
+  stat$time <- as.numeric(gsub("\\D", "", stat$time))
+  class(stat$value) <- "numeric"
+  stat
+}
+
