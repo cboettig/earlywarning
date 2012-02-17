7e4bb4c84c269b9e2b70e36277bee4c61928ccc1
Carl Boettiger <cboettig@gmail.com>
2012-02-16 18:57:35 -0800

    fallacy sim introduced

--- demo/prosecutorSims.R
+++ demo/prosecutorSims.R
@@ -3,11 +3,11 @@
 ##Simulate a dataset from the full individual, nonlinear model
 rm(list=ls())
 T<- 5000
-n_pts <- 500000
+n_pts <- 50000
 require(populationdynamics)
 # Stable example parameters
 pars = c(Xo = 500, e = 0.5, a = 180, K = 1000, h = 200,
     i = 0, Da = 0, Dt = 0, p = 2)
 ## Run the individual based simulation
-sn <- saddle_node_ibm(pars, times=seq(0,T, length=n_pts), reps=10000)
+sn <- saddle_node_ibm(pars, times=seq(0,T, length=n_pts), reps=1)
 save("sn", "file=prosecutor.rda")
