1f7946c4250cafe7015a4401336e0de54ba45531
Carl Boettiger <cboettig@gmail.com>
2012-02-20 11:53:55 -0800

    autocommit

--- demo/prosecutorSims.R
+++ demo/prosecutorSims.R
@@ -1,13 +1,13 @@
 # 
 
 ##Simulate a dataset from the full individual, nonlinear model
-rm(list=ls())
-T<- 5000
-n_pts <- 50000
-require(populationdynamics)
+#rm(list=ls())
+#T<- 5000
+#n_pts <- 50000
+#require(populationdynamics)
 # Stable example parameters
-pars = c(Xo = 500, e = 0.5, a = 180, K = 1000, h = 200,
-    i = 0, Da = 0, Dt = 0, p = 2)
+#pars = c(Xo = 500, e = 0.5, a = 180, K = 1000, h = 200,
+#    i = 0, Da = 0, Dt = 0, p = 2)
 ## Run the individual based simulation
 #sn <- saddle_node_ibm(pars, times=seq(0,T, length=n_pts), reps=1)
 #save("sn", "file=prosecutor.rda")
@@ -22,45 +22,55 @@ pars = c(Xo = 500, e = 0.5, a = 180, K = 1000, h = 200,
 #rm(list=ls())
 load("crashed.rda")
 
-L <- length(unique(dat$reps))
 
-library(snow)
+## Zoom in on the relevant area of data near the crash
+require(plyr)
+zoom <- ddply(dat, "reps", function(X){
+    tip <- min(which(X$value==0))
+    index <- max(tip-800,1):tip
+    data.frame(time=X$time[index], value=X$value[index])
+    })
+
+require(ggplot2)
+ggplot(subset(zoom, reps < 10 & value > 300)) + geom_line(aes(time, value)) + facet_wrap(~reps, scales="free")
+
+
+#L <- length(unique(dat$reps))
+
+#library(snow)
 ## snow method
-cluster <- makeCluster(80, type="MPI")
-clusterEvalQ(cluster, library(earlywarning)) # load a library
-clusterExport(cluster, ls()) # export everything in workspace
-models <- parLapply(cluster, 1:L, function(i)
-  stability_model(dat[dat$rep==i, c("time", "population")], "LSN")
-  )
-stopCluster(cluster)
-save("models", file="models.rda")
+#cluster <- makeCluster(80, type="MPI")
+#clusterEvalQ(cluster, library(earlywarning)) # load a library
+#clusterExport(cluster, ls()) # export everything in workspace
+#models <- parLapply(cluster, 1:L, function(i)
+#  stability_model(dat[dat$rep==i, c("time", "population")], "LSN")
+#  )
+#stopCluster(cluster)
+#save("models", file="models.rda")
 
 
+#
+## plot indicators
+#require(plyr)
+#require(earlywarning)
+#indicators <- ddply(zoom, "reps", function(X){
+#    Y <- data.frame(time=X$time[index], value=X$value[index])
+#    tau <- warningtrend(Y, window_var)
+#    i <- X$rep[1]
+#    m <- models[[i]]$pars["m"]
+#    c(tau, m)
+#})
+#
+#require(beanplot)
+#png("beanplot.png", width=480*2)
+#par(mfrow=c(1,2))
+#beanplot(m, indicators, what=c(0,1,0,0))
+#beanplot(kendall_coef, subset(indicators, abs(m) <1), what=c(0,1,0,0))
+#dev.off()
+#
 
-runlater <- function(){
-indicators <- ddply(dat, "reps", function(X){ 
-    Y <- data.frame(X$time, X$value)
-    tau <- compute_tau(Y, "Var")[1]
-    i <- X$rep[1]
-    m <- models[[i]]$pars["m"]
-    c(tau, m)
-})
-
-require(beanplot)
-png("beanplot.png", width=480*2)
-par(mfrow=c(1,2))
-beanplot(m, indicators, what=c(0,1,0,0))
-beanplot(kendall_coef, subset(indicators, abs(m) <1), what=c(0,1,0,0))
-dev.off()
-
-
-ggplot(indicators) + geom_density(aes(kendall_coef))
-ggplot(indicators) + geom_density(aes(m))
-}
-
-
-
-
+#ggplot(indicators) + geom_density(aes(kendall_coef))
+#ggplot(indicators) + geom_density(aes(m))
 
 
 
